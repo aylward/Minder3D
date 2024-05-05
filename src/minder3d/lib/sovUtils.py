46467366f52efe08fc52a177944d2cee22bea2fe
aylward <aylward@users.noreply.github.com>
2024-05-05 20:39:44 +0000

    STYLE:Fixes by auto-format

--- src/minder3d/lib/sovUtils.py
+++ src/minder3d/lib/sovUtils.py
@@ -18,7 +18,7 @@ import uuid
 
 import itk
 import numpy as np
-from PySide6.QtCore import QCoreApplication, QSettings, QStandardPaths
+from PySide6.QtCore import QSettings, QStandardPaths
 from PySide6.QtWidgets import QMainWindow, QTextEdit
 
 from .sovColorMapUtils import short_colormap, short_colormap_scale_factor
