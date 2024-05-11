9bfb578af4d196e325d87607ee1a8971a2eda9d3
aylward <aylward@users.noreply.github.com>
2024-05-11 22:56:53 +0000

    STYLE:Fixes by auto-format

--- src/minder3d/lib/sovView2DPanelWidget.py
+++ src/minder3d/lib/sovView2DPanelWidget.py
@@ -7,13 +7,6 @@ from vtk.util.numpy_support import vtk_to_numpy
 
 from .sovUtils import time_and_log
 from .sovView2DRenderWindowInteractor import View2DRenderWindowInteractor
-from .sovView2DResources import (
-    qCleanupResources,
-    qInitResources,
-    qt_resource_data,
-    qt_resource_name,
-    qt_resource_struct,
-)
 from .sovView2DUtils import (
     render_object_in_overlay_array,
     render_scene_in_overlay_array,
