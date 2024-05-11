cbc94a7715e4693dc5bb28aa8cdabb5e4eb31ad2
Stephen Aylward <stephen.aylward@kitware.com>
2024-05-11 18:56:09 -0400

    STYLE: Precommit formatting

--- src/minder3d/lib/sovView2DPanelWidget.py
+++ src/minder3d/lib/sovView2DPanelWidget.py
@@ -8,11 +8,11 @@ from vtk.util.numpy_support import vtk_to_numpy
 from .sovUtils import time_and_log
 from .sovView2DRenderWindowInteractor import View2DRenderWindowInteractor
 from .sovView2DResources import (
+    qCleanupResources,
+    qInitResources,
     qt_resource_data,
     qt_resource_name,
     qt_resource_struct,
-    qInitResources,
-    qCleanupResources
 )
 from .sovView2DUtils import (
     render_object_in_overlay_array,
